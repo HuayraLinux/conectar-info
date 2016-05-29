242ecad961b90ed23d8f92a7e73a4c202dcaa3b9
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-05-29 18:40:44 -0300

    Creando el service tracker para estadísticas.

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,9 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   tray: Ember.inject.service(),
+  tracker: Ember.inject.service(),
 
   activate() {
     this.get("tray");
+    this.get("tracker");
   }
 
 });
