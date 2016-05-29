0e9072ac8f99ab86a2acb283991d2d3a20836c0d
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-05-29 18:19:36 -0300

    Implementando gaze para live-reloading.

--- vendor/nwjs_fixes.js
+++ vendor/nwjs_fixes.js
@@ -19,9 +19,11 @@ var DELAY = 2000;
 var refresh = false;
 
 if (is_nodewebkit) {
-  var fs = requireNode('fs');
+  var Gaze = requireNode('gaze').Gaze;
+  var gaze = new Gaze('**/*');
 
-  fs.watchFile('index.html', function() {
+  gaze.on('all', function(event, filepath) {
+
     if (refresh === false) {
       refresh = true;
       console.log("Detectando cambio en el archivo index.html, actualizando ...");
@@ -29,5 +31,8 @@ if (is_nodewebkit) {
         requireNode('nw.gui').Window.get().reloadIgnoringCache();
       }, DELAY);
     }
+
+  });
+}
   });
 }
